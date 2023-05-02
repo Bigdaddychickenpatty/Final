4b8132b988d75a8d2e81dec403357e0ccd892636
Bigdaddychickenpatty <111007609+Bigdaddychickenpatty@users.noreply.github.com>
2023-05-02 11:04:41 -0700

    camera attempt

--- Final/Assets/Scripts/SpawnManager.cs
+++ Final/Assets/Scripts/SpawnManager.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera : MonoBehaviour
+public class SpawnManager : MonoBehaviour
 {
-    
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
-    
 
     // Update is called once per frame
     void Update()
     {
-   
+        
     }
 }
