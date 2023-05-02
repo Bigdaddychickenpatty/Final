19b5671aa2703adb8c0eb8253d119d13bf0e5c3b
Bigdaddychickenpatty <111007609+Bigdaddychickenpatty@users.noreply.github.com>
2023-05-02 12:51:24 -0700

    enemy range

--- Final/Assets/Scripts/Enemies/Enemy.cs
+++ Final/Assets/Scripts/Enemies/Enemy.cs
@@ -9,6 +9,8 @@ public class Enemy : MonoBehaviour
     public int Speed = 1;
     private Rigidbody _enemyRb;
     public GameObject _player;
+    public float Range;
+    public Transform player;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +21,15 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(target);
-       Vector3 localposition = _player.transform.position - transform.position;
-       localposition = localposition.normalized; // The normalized direction in LOCAL space
-       transform.Translate(-localposition.x * Time.deltaTime * Speed, -localposition.y * Time.deltaTime * Speed, -localposition.z * Time.deltaTime * Speed);
+        if(Vector3.Distance(player.position, transform.position) <= Range)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.position, Speed * Time.deltaTime);
+        }
 
-       
+        transform.LookAt(target);
+       //Vector3 localposition = _player.transform.position - transform.position;
+       //localposition = localposition.normalized; // The normalized direction in LOCAL space
+       //transform.Translate(-localposition.x * Time.deltaTime * Speed, -localposition.y * Time.deltaTime * Speed, -localposition.z * Time.deltaTime * Speed);
     }
 
     private void OnTriggerEnter(Collider other)
--- Final/Assets/Scripts/SpawnManager.cs
+++ Final/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject EnemyPrefab;
+    public float XRange = 5.5f;
+    public float YRange = 2.5f;
+    public int EnemyCount = 0;
+    public int WaveCount = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnEnemyWave(WaveCount);
     }
 
     // Update is called once per frame
     void Update()
     {
+        EnemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(EnemyCount == 0)
+        {
+            WaveCount++; // WaveCount = WaveCount + 1;
+            SpawnEnemyWave(WaveCount);
+        }
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-XRange, XRange);
+        float spawnPosY = Random.Range(-YRange, YRange);
+        Vector3 randomPos = new Vector3(spawnPosX, spawnPosY);
+        return randomPos;
+    }
+
+    private void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(EnemyPrefab, GenerateSpawnPosition(), EnemyPrefab.transform.rotation);
+        }
         
     }
 }
